b95f8f50b8029a96f71be078b25445896c4d77f0
Yaswanth Kakarla <yaswanth.kakarla@agnity.com>
2014-04-16 13:05:01 +0530

    Handled the FaceBook Authentication Failure & Minor changes to view

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,3 +3,7 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '626748084079789', '52e485e6789127d8f25d092d39ce65c9', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
 end
+
+OmniAuth.config.on_failure = Proc.new { |env|
+  OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+}
